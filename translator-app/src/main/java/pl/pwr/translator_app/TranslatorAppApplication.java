3f7c8d910026f8d0426db1e4f2600da13cab7e2a
durki <michal@durkalec.com>
2025-03-24 21:47:22 +0100

    Add json2sql as dependency in spring app

--- translator-app/src/main/java/pl/pwr/translator_app/TranslatorAppApplication.java
+++ translator-app/src/main/java/pl/pwr/translator_app/TranslatorAppApplication.java
@@ -2,6 +2,7 @@ package pl.pwr.translator_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.pwr.parser.QueryTranslator;
 
 @SpringBootApplication
 public class TranslatorAppApplication {
